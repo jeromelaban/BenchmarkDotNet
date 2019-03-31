8d075c195d64731a82a2b10b4f2ebcc5d85d8073
Jérôme Laban <jerome.laban@nventive.com>
2019-03-30 22:32:28 -0400

    Adjust default sample

--- samples/BenchmarkDotNet.Live/BenchmarkDotNet.Live.Shared/Sources/DefaultContent.cs
+++ samples/BenchmarkDotNet.Live/BenchmarkDotNet.Live.Shared/Sources/DefaultContent.cs
@@ -7,10 +7,15 @@
 // use BenchmarkDotNet to measure the performance of each method marked with the
 // [Benchmark] attribute.
 //
-// Note that WebAssembly currently does not support threading (support is coming), which
-// means that the page will freeze during the run of the benchmarks.
+// Note that :
+//   - WebAssembly currently does not support threading (support is coming), which
+//     means that the page will freeze during the run of the benchmarks. 
+//   - The whole app payload is quite large as it contains a full compiler and 
+//     libraries, and the full mscorlib AOT'ed to allow you to test more of the 
+//     framework. This generally is not required in a normal app.
+//   - It gives a hard time to Chrome 73, but works a *lot* better in Chrome 75.
 //
-// See https://benchmarkdotnet.org/articles/samples/IntroBasic.html for more samples !
+// See https://benchmarkdotnet.org/articles/samples/IntroBasic.html for more benchmarks !
 
 using System;
 using System.Security.Cryptography;
@@ -21,7 +26,7 @@ namespace MyBenchmarks
 {
     public class Md5VsSha256
     {
-        private const int N = 1000;
+        private const int N = 10000;
         private readonly byte[] data;
 
         private readonly SHA256 sha256 = SHA256.Create();
